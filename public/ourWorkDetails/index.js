f105f3c68b24a1e2835702f9528a0c3e9fb81276
ismaila <ismail@ideabox.pk>
2023-05-08 21:00:50 +0500

    fix

--- public/ourWorkDetails/index.js
+++ public/ourWorkDetails/index.js
@@ -3,8 +3,8 @@ import bycologo from "./bycologo.png";
 import rently from "./rently.png";
 import zod from "./zod.png";
 import tb from "./tb.png";
-import mediacenter from "./mediacenter.png";
-import zahraebrahim from "./zahraebrahim.png";
+import mediacenter from "./mediacenter.PNG";
+import zahraebrahim from "./zahraebrahim.PNG";
 import oshoot from "./oshoot.png";
 
 export const images = {
