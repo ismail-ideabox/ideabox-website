1358acfd1f2a6f3c017e5da0bc38c1b039839eac
zain sheikh <zaynsheikh881@gmail.com>
2024-05-16 19:26:51 +0500

    changes

--- src/app/api/mailer/route.js
+++ src/app/api/mailer/route.js
@@ -1,37 +1,31 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-async function verifyCaptchaAction(token) {
-  const res = await fetch(
+export async function verifyCaptchaAction(token) {
+  const response = await fetch(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`,
     {
       method: "POST",
-      mode: "no-cors",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
       body: `secret=${process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY}&response=${token}`,
     }
   );
-  if (res?.data?.score > 0.5) {
-    return true;
-  } else {
-    return false;
-  }
+  const data = await response.json();
+  return data?.score > 0.5;
 }
 
 export async function POST(req, res) {
   const body = await req.json();
 
   const { name, companyName, email, phone, message, gReCaptchaToken } = body;
-  const verify = verifyCaptchaAction();
+
+  const verify = await verifyCaptchaAction(gReCaptchaToken);
 
   if (verify) {
     let transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: "noreply.ideabox.pk@gmail.com",
-        pass: "hkes sazq bouz shhh",
+        pass: "vpsy cmrh bxhe lhlk",
       },
     });
     let mailOptions = {
@@ -53,11 +47,23 @@ export async function POST(req, res) {
           headers: { "Content-Type": "application/json" },
         }
       );
-    } catch (error) {}
+    } catch (error) {
+      return new NextResponse(
+        JSON.stringify({
+          status: "error",
+          message: error.message,
+        }),
+        {
+          status: 500,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+  } else {
     return new NextResponse(
       JSON.stringify({
         status: "error",
-        message: error,
+        message: "recaptcha token is invalid ",
       }),
       {
         status: 500,
